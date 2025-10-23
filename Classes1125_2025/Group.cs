f5c48e52ce7c3fa5106c29b8d7ede203e01aa184
stenly87 <stenly87@bk.ru>
2025-10-23 10:08:46 +1000

    Распихали классы по разным файлам

---
+++ Classes1125_2025/Group.cs
@@ -0,0 +1,40 @@
+﻿// классы располагаются в пространствах имен
+namespace TestNamespace
+{
+    public class Group
+    {
+        // для простого примера используем массив
+        // хотя данные могут храниться как угодно
+        string[] students = new string[30];
+
+        public int StudentCount
+        {
+            get
+            {
+                return students.Length;
+            }
+        }
+        // пример индексатора
+        // индексатор возвращает и получает тип string
+        // но можно использовать любые типы, от программиста
+        // зависит то, как будет генерироваться ответ
+        // и как будет обрабатываться получаемое значение
+        public string this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= students.Length)
+                    throw new Exception("Неверный индекс");
+                return students[index];
+            }
+            set
+            {
+                if (index < 0 || index >= students.Length)
+                    throw new Exception("Неверный индекс");
+                students[index] = value;
+            }
+        }
+
+
+    }
+}
---
+++ Classes1125_2025/Table.cs
@@ -0,0 +1,42 @@
+﻿// классы располагаются в пространствах имен
+namespace TestNamespace
+{
+    public class Table
+    {
+        private string title;
+
+        // короткая запись свойства 
+        // для случаев, когда нет смысла проверять 
+        // назначаемое значение или вычислять возращаемое
+        public string Title {
+            get => title;
+            set
+            {
+                title = value;
+                // вызов события с передачей аргументов
+                // this - ссылка на текущий объект
+                // this.title - новое значение
+                // ?.Invoke - запуск события 
+                // с проверкой на наличие подписчиков
+                // если их нет, то событие не произойдет
+                TitleChanged?.Invoke(this, this.title);
+            }
+        }
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public string Color { get; set; }
+
+        // деструктор
+        ~Table()
+        {
+            Console.WriteLine("Сработал деструктор объекта Table");
+            Console.WriteLine(Title);
+        }
+
+        // объявление события
+        // на событие подписываются объекты извне (также как на делегаты)
+        // но вызвать событие может только класс
+        // внутри которого оно объявляено (в отличие от делегата)
+        public event EventHandler<string> TitleChanged;
+    }
+}
